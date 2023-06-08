640188d419ea86e7bf6ac142fdd273d10ce5ddd4
류선영 <84279479+tjsdud594@users.noreply.github.com>
2023-06-08 10:10:17 +0900

    Add files via upload

--- dw_Example.py
+++ dw_Example.py
@@ -26,6 +26,8 @@ https://github.com/GoogleCloudPlatform/spark-on-k8s-operator
 """
 
 from datetime import timedelta, datetime
+from typing import Dict
+import boto3
 
 # [START import_module]
 # The DAG object; we'll need this to instantiate a DAG
@@ -33,10 +35,45 @@ from airflow import DAG
 # Operators; we need this to operate!
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.providers.trino.operators.trino import TrinoOperator
 from airflow.utils.dates import days_ago
 from kubernetes.client import models as k8s
 
 # [END import_module]
+
+
+def get_file_from_s3(s3_client, Bucket, Key) -> Dict:
+    """ 
+    S3 파일을 읽어서 utf-8 디코딩한 데이터를 반환합니다.
+    단, 파일이 UTF-8로 작성되어 있어야 정상적으로 파일을 읽을 수 있음.
+    Args:
+        s3_client: boto3 s3 클라이언트
+        Bucket: 버킷명
+        Key: 오브젝트 키
+    Returns:
+        data_dict
+    Raises:
+        None
+    Example:
+        import boto3
+        s3_client = boto3.client("s3")    
+        bucket = "hr-rcmd-stg"
+        key_sql = "redshift/sql/casting_query/inst1_tskxban01.sql"
+        sql_file = get_file_from_s3(s3_client, bucket, key_sql)
+        print(sql_file)
+        print(type(sql_file)) # str
+    """
+    try:
+        data = s3_client.get_object(Bucket=Bucket, Key=Key)
+        file = data['Body'].read().decode('utf-8')
+    except Exception as e:
+        print(f"Error Occurred get_file_from_s3: {e}")
+        raise e
+    return file
+
+s3_client = boto3.client("s3")
+Bucket = "imgr-buc-inner"
+Key = "trino_sql/tier2_join.sql"
 
 # [START default_args]
 # These args will get passed on to each operator
@@ -69,7 +106,7 @@ dag = DAG(
 )
 
 t0 = SparkKubernetesOperator(
-    task_id='Tier_0',
+    task_id='Tier_0_spark',
     namespace="guru-tenant",
     application_file="t0_spark.yaml",
     kubernetes_conn_id="guru",
@@ -89,7 +126,7 @@ t0_sensor = SparkKubernetesSensor(
 )
 
 t1 = SparkKubernetesOperator(
-    task_id='Tier_1',
+    task_id='Tier_1_spark',
     namespace="guru-tenant",
     application_file="t1_spark.yaml",
     kubernetes_conn_id="guru",
@@ -108,4 +145,11 @@ t1_sensor = SparkKubernetesSensor(
     api_group="sparkoperator.hpe.com"
 )
 
-t0 >> t0_sensor >> t1 >> t1_sensor
+t2 = TrinoOperator(
+        task_id="Tier_2_trino",
+        trino_conn_id="trino_hive",
+        sql=get_file_from_s3(s3_client, Bucket, Key),
+        handler=list
+    )
+
+t0 >> t0_sensor >> t1 >> t1_sensor >> t2
